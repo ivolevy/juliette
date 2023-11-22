bf17c2e167bd9a6b5f365fd90a2c032c47172332
Ivan Levy <ivanlevy@MacBook-Air-de-Ivan.local>
2023-11-22 20:48:14 -0300

    a

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,8 +3,8 @@ import Employee from "./Employee";
 import Details from "./Details";
 const people = [
   {
-    name: "Leslie Alexander",
-    email: "leslie.alexander@example.com",
+    name: "Iñaki Moreno",
+    email: "imoreno@uade.edu.ar",
     role: "Co-Founder / CEO",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
@@ -12,8 +12,8 @@ const people = [
     lastSeenDateTime: "2023-01-23T13:23Z",
   },
   {
-    name: "Michael Foster",
-    email: "michael.foster@example.com",
+    name: "Fran Demare",
+    email: "fdemare@uade.edu.ar",
     role: "Co-Founder / CTO",
     imageUrl:
       "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
